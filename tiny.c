e26baf910ffd2b47dc79b6f36c416897dae7ee3a
kittorjk <36001612+kittorjk@users.noreply.github.com>
2020-03-08 22:50:32 -0400

    Update tiny.c

--- tiny.c
+++ tiny.c
@@ -6,13 +6,17 @@
 #include "csapp.h"
 
 void doit(int fd);
-void read_requesthdrs(rio_t *rp);
+int read_requesthdrs(rio_t *rp, char *method);
 int parse_uri(char *uri, char *filename, char *cgiargs);
 void serve_static(int fd, char *filename, int filesize);
 void get_filetype(char *filename, char *filetype);
 void serve_dynamic(int fd, char *filename, char *cgiargs);
 void clienterror(int fd, char *cause, char *errnum, 
 		 char *shortmsg, char *longmsg);
+void sanitize_uri(char *uri);
+void whitelist_display(char *filename);
+void whitelist_binaries(char *filename);
+void sigchld_handler(int sig);
 
 int main(int argc, char **argv) 
 {
@@ -21,20 +25,35 @@ int main(int argc, char **argv)
 
     /* Check command line args */
     if (argc != 2) {
-	fprintf(stderr, "usage: %s <port>\n", argv[0]);
-	exit(1);
+	    fprintf(stderr, "usage: %s <port>\n", argv[0]);
+	    exit(1);
     }
     port = atoi(argv[1]);
 
+    Signal(SIGCHLD, sigchld_handler);
     listenfd = Open_listenfd(port);
     while (1) {
-	clientlen = sizeof(clientaddr);
-	connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen); //line:netp:tiny:accept
-	doit(connfd);                                             //line:netp:tiny:doit
-	Close(connfd);                                            //line:netp:tiny:close
+        clientlen = sizeof(clientaddr);
+        connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen); //line:netp:tiny:accept
+        //doit(connfd);                                             //line:netp:tiny:doit
+        //Close(connfd);                                            //line:netp:tiny:close
+
+        if (Fork() == 0) { //New child process
+            Close(listenfd); /* Close child listening socket */
+            doit(connfd);
+            Close(connfd);
+            exit(0); /* Child exits */
+        }
+        Close(connfd); /* Close parent socket */
     }
 }
 /* $end tinymain */
+
+/* Manage zombie processes */
+void sigchld_handler(int sig) {
+    while (waitpid(-1, 0, WNOHANG) > 0);
+    return;
+}
 
 /*
  * doit - handle one HTTP request/response transaction
@@ -44,44 +63,58 @@ void doit(int fd)
 {
     int is_static;
     struct stat sbuf;
-    char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+    char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE], data[MAXLINE];
     char filename[MAXLINE], cgiargs[MAXLINE];
+    int data_length;
     rio_t rio;
   
     /* Read request line and headers */
     Rio_readinitb(&rio, fd);
     Rio_readlineb(&rio, buf, MAXLINE);                   //line:netp:doit:readrequest
     sscanf(buf, "%s %s %s", method, uri, version);       //line:netp:doit:parserequest
-    if (strcasecmp(method, "GET")) {                     //line:netp:doit:beginrequesterr
-       clienterror(fd, method, "501", "Not Implemented",
+    if (strcasecmp(method, "GET") && strcasecmp(method, "POST")) {                     //line:netp:doit:beginrequesterr
+        clienterror(fd, method, "501", "Not Implemented",
                 "Tiny does not implement this method");
         return;
     }                                                    //line:netp:doit:endrequesterr
-    read_requesthdrs(&rio);                              //line:netp:doit:readrequesthdrs
+    /* Read headers and obtain length of data sent if method is POST */
+    data_length = read_requesthdrs(&rio, method);                              //line:netp:doit:readrequesthdrs
 
-    /* Parse URI from GET request */
+    /* Retrieve data sent if using POST */
+    Rio_readnb(&rio, data, data_length);
+    data[data_length] = '\0';                            // Add NULL at the end of data to not copy old memory values
+
+    /* Parse URI from request */
     is_static = parse_uri(uri, filename, cgiargs);       //line:netp:doit:staticcheck
     if (stat(filename, &sbuf) < 0) {                     //line:netp:doit:beginnotfound
-	clienterror(fd, filename, "404", "Not found",
-		    "Tiny couldn't find this file");
-	return;
+        clienterror(fd, filename, "404", "Not found",
+                "Tiny couldn't find this file");
+        return;
     }                                                    //line:netp:doit:endnotfound
 
     if (is_static) { /* Serve static content */          
-	if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)) { //line:netp:doit:readable
-	    clienterror(fd, filename, "403", "Forbidden",
-			"Tiny couldn't read the file");
-	    return;
-	}
-	serve_static(fd, filename, sbuf.st_size);        //line:netp:doit:servestatic
+        if (!strcasecmp(method, "POST")) {     /* POST method is only allowed in dynamic version of tiny */
+            clienterror(fd, filename, "501", "Not Implemented",
+                "Cannot POST to the specified uri");
+            return;
+        }
+        if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)) { //line:netp:doit:readable
+            clienterror(fd, filename, "403", "Forbidden",
+                "Tiny couldn't read the file");
+            return;
+        }
+        serve_static(fd, filename, sbuf.st_size);        //line:netp:doit:servestatic
     }
     else { /* Serve dynamic content */
-	if (!(S_ISREG(sbuf.st_mode)) || !(S_IXUSR & sbuf.st_mode)) { //line:netp:doit:executable
-	    clienterror(fd, filename, "403", "Forbidden",
-			"Tiny couldn't run the CGI program");
-	    return;
-	}
-	serve_dynamic(fd, filename, cgiargs);            //line:netp:doit:servedynamic
+        if (!(S_ISREG(sbuf.st_mode)) || !(S_IXUSR & sbuf.st_mode)) { //line:netp:doit:executable
+            clienterror(fd, filename, "403", "Forbidden",
+                "Tiny couldn't run the CGI program");
+            return;
+        }
+        if (!strcasecmp(method, "POST"))
+            serve_dynamic(fd, filename, data);              // Use data from POST body
+        else
+            serve_dynamic(fd, filename, cgiargs);            //line:netp:doit:servedynamic
     }
 }
 /* $end doit */
@@ -90,16 +123,21 @@ void doit(int fd)
  * read_requesthdrs - read and parse HTTP request headers
  */
 /* $begin read_requesthdrs */
-void read_requesthdrs(rio_t *rp) 
+int read_requesthdrs(rio_t *rp, char *method) 
 {
     char buf[MAXLINE];
+    int data_length = 0;
 
     Rio_readlineb(rp, buf, MAXLINE);
     while(strcmp(buf, "\r\n")) {          //line:netp:readhdrs:checkterm
-	Rio_readlineb(rp, buf, MAXLINE);
-	printf("%s", buf);
+        Rio_readlineb(rp, buf, MAXLINE);
+        printf("%s", buf);
+        /* If method is POST get value of Content-Length header */
+        if (!strcasecmp(method, "POST") && !strncasecmp(buf, "Content-Length:", 15))
+            sscanf(buf, "Content-Length: %d", &data_length);
     }
-    return;
+
+    return data_length;
 }
 /* $end read_requesthdrs */
 
@@ -112,25 +150,29 @@ int parse_uri(char *uri, char *filename, char *cgiargs)
 {
     char *ptr;
 
+    sanitize_uri(uri);
+
     if (!strstr(uri, "cgi-bin")) {  /* Static content */ //line:netp:parseuri:isstatic
-	strcpy(cgiargs, "");                             //line:netp:parseuri:clearcgi
-	strcpy(filename, ".");                           //line:netp:parseuri:beginconvert1
-	strcat(filename, uri);                           //line:netp:parseuri:endconvert1
-	if (uri[strlen(uri)-1] == '/')                   //line:netp:parseuri:slashcheck
-	    strcat(filename, "home.html");               //line:netp:parseuri:appenddefault
-	return 1;
+	    strcpy(cgiargs, "");                             //line:netp:parseuri:clearcgi
+	    strcpy(filename, ".");                           //line:netp:parseuri:beginconvert1
+	    strcat(filename, uri);                           //line:netp:parseuri:endconvert1
+	    if (uri[strlen(uri)-1] == '/')                   //line:netp:parseuri:slashcheck
+	        strcat(filename, "home.html");               //line:netp:parseuri:appenddefault
+        whitelist_display(filename);
+	    return 1;
     }
     else {  /* Dynamic content */                        //line:netp:parseuri:isdynamic
-	ptr = index(uri, '?');                           //line:netp:parseuri:beginextract
-	if (ptr) {
-	    strcpy(cgiargs, ptr+1);
-	    *ptr = '\0';
-	}
-	else 
-	    strcpy(cgiargs, "");                         //line:netp:parseuri:endextract
-	strcpy(filename, ".");                           //line:netp:parseuri:beginconvert2
-	strcat(filename, uri);                           //line:netp:parseuri:endconvert2
-	return 0;
+        ptr = index(uri, '?');                           //line:netp:parseuri:beginextract
+        if (ptr) {
+            strcpy(cgiargs, ptr+1);
+            *ptr = '\0';
+        }
+        else 
+            strcpy(cgiargs, "");                         //line:netp:parseuri:endextract
+        strcpy(filename, ".");                           //line:netp:parseuri:beginconvert2
+        strcat(filename, uri);                           //line:netp:parseuri:endconvert2
+        whitelist_binaries(filename);
+        return 0;
     }
 }
 /* $end parse_uri */
@@ -226,3 +268,53 @@ void clienterror(int fd, char *cause, char *errnum,
     Rio_writen(fd, body, strlen(body));
 }
 /* $end clienterror */
+
+void sanitize_uri(char *uri)
+{
+    char *tmp;
+    int len = strlen(uri);
+    while ((tmp = strstr(uri, "/.."))) {
+        *tmp = '\0';
+        strcat(uri, tmp+len);
+    }
+}
+
+void whitelist_display(char *filename) {
+    const char *allowed[2];
+    allowed[0] = "./home.html";
+    allowed[1] = "./README";
+    int match_found = 0;
+    size_t i = 0;
+
+    while (allowed[i] != '\0') {       /* Stop looping when we reach the null-character. */
+        if (strstr(filename, allowed[i])) {
+            match_found = 1;
+            break;                      // Exit the loop if a match is found
+        }
+        i++;
+    }
+
+    if (match_found == 0) {
+        strcpy(filename, "./home.html"); // If no match is found home.html is returned
+    }
+}
+
+void whitelist_binaries(char *filename) {
+    const char *allowedb[2];
+    allowedb[0] = "adder";
+    allowedb[1] = "test";
+    int matchb_found = 0;
+    size_t j = 0;
+
+    while (allowedb[j] != '\0') {       /* Stop looping when we reach the null-character. */
+        if (strstr(filename, *allowedb)) {
+            matchb_found = 1;
+            break;                      // Exit the loop if a match is found
+        }
+        j++;
+    }
+
+    if (matchb_found == 0) {
+        strcpy(filename, ""); // If no match is found adder is returned
+    }
+}
